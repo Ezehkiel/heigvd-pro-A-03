813185ead1dc03c40ba9df0a8f48564493575949
Didier Page <page.didier@gmail.com>
2019-03-25 11:26:41 +0100

    <feate> Add the server file structure

--- towerdefense/towerdefense-server/src/main/java/ch/heigvd/pro/a03/Server.java
+++ towerdefense/towerdefense-server/src/main/java/ch/heigvd/pro/a03/Server.java
@@ -1,14 +1,21 @@
 package ch.heigvd.pro.a03;
 
-import static spark.Spark.*;
+
+import ch.heigvd.pro.a03.httpServer.HttpServer;
+import ch.heigvd.pro.a03.socketServer.MultiThreadedServer;
 
 public class Server {
 
-    public Server() {
-        get("/hello/:name", (request, response) -> HelloWorld.greet(request.params(":name")));
-    }
-
     public static void main(String[] args) {
+
+
+
         new Server();
+
+
+        // Run HTTP on other thread;
+
+        new Thread(new HttpServer(Protocole.HTTPSERVERPORT)).start();
+        new Thread(new MultiThreadedServer(Protocole.SOCKETSERVERPORT)).start();
     }
 }
---
+++ towerdefense/towerdefense-server/src/main/java/ch/heigvd/pro/a03/httpServer/HttpServer.java
@@ -0,0 +1,23 @@
+package ch.heigvd.pro.a03.httpServer;
+
+import ch.heigvd.pro.a03.HelloWorld;
+import ch.heigvd.pro.a03.socketServer.MultiThreadedServer;
+
+import java.util.logging.Logger;
+import static spark.Spark.*;
+
+public class HttpServer implements Runnable {
+    final static Logger LOG = Logger.getLogger(MultiThreadedServer.class.getName());
+    int port;
+
+    public HttpServer(int port) {
+        port(port);
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        LOG.info("Starting the http server on port :"+this.port);
+        get("/hello/:name", (request, response) -> HelloWorld.greet(request.params(":name")));
+    }
+}
