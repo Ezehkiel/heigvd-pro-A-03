f5acc4b8d34eba62b12eb3076a820e1bb252a999
Didier Page <page.didier@gmail.com>
2019-03-27 15:13:50 +0100

    <feat> add constructors in WarEntity hierarchy

--- towerdefense/towerdefense-core/src/main/java/ch/heigvd/pro/a03/warentities/Base.java
+++ towerdefense/towerdefense-core/src/main/java/ch/heigvd/pro/a03/warentities/Base.java
@@ -2,5 +2,11 @@ package ch.heigvd.pro.a03.warentities;
 
 import ch.heigvd.pro.a03.warentities.Structure;
 
+import java.awt.*;
+
 public class Base extends Structure {
+
+    public Base(String name, Point position) {
+        super(name, position);
+    }
 }
--- towerdefense/towerdefense-core/src/main/java/ch/heigvd/pro/a03/warentities/units/Unit.java
+++ towerdefense/towerdefense-core/src/main/java/ch/heigvd/pro/a03/warentities/units/Unit.java
@@ -4,6 +4,10 @@ import ch.heigvd.pro.a03.utils.FloatPoint;
 import ch.heigvd.pro.a03.warentities.WarEntity;
 
 public class Unit extends WarEntity {
+    public Unit(String name,FloatPoint position) {
+        super(name);
+        this.position = position;
+    }
 
     private FloatPoint position;
 }
