29934eeda1a28143a8960a157b51be64462817fd
Remi Poulard <remi.poulard@heig-vd.ch>
2019-03-29 09:25:49 +0100

    WIP auth players

---
+++ towerdefense/towerdefense-server/src/main/java/ch/heigvd/pro/a03/httpServer/AuthUser.java
@@ -0,0 +1,13 @@
+package ch.heigvd.pro.a03.httpServer;
+
+import com.google.gson.Gson;
+
+public class AuthUser {
+
+    public static boolean isValide(String body) {
+        Gson g = new Gson();
+        System.out.println(body);
+
+        return false;
+    }
+}
--- towerdefense/towerdefense-server/src/main/java/ch/heigvd/pro/a03/httpServer/HttpServer.java
+++ towerdefense/towerdefense-server/src/main/java/ch/heigvd/pro/a03/httpServer/HttpServer.java
@@ -2,6 +2,8 @@ package ch.heigvd.pro.a03.httpServer;
 
 import ch.heigvd.pro.a03.HelloWorld;
 import ch.heigvd.pro.a03.socketServer.MultiThreadedServer;
+import com.google.gson.Gson;
+import spark.ResponseTransformer;
 
 import java.util.logging.Logger;
 import static spark.Spark.*;
@@ -9,15 +11,19 @@ import static spark.Spark.*;
 public class HttpServer implements Runnable {
     final static Logger LOG = Logger.getLogger(MultiThreadedServer.class.getName());
     int port;
+    private Gson gson = new Gson();
 
     public HttpServer(int port) {
         port(port);
         this.port = port;
+
     }
 
     @Override
     public void run() {
-        LOG.info("Starting the http server on port :"+this.port);
+        LOG.info("Starting the http server on port :" + this.port);
         get("/hello/:name", (request, response) -> HelloWorld.greet(request.params(":name")));
+        post("/user/login", (request, response) -> AuthUser.isValide(request.body()));
+
     }
 }
