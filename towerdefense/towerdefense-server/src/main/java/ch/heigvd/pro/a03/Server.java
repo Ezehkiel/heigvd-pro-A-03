83232813232826c003e0aa28024cea4b8c6a1a09
Remi Poulard <remi.poulard@heig-vd.ch>
2019-05-06 15:51:16 +0200

    Enable https and move directory deploy in towerdefense-server/

--- towerdefense/towerdefense-server/src/main/java/ch/heigvd/pro/a03/Server.java
+++ towerdefense/towerdefense-server/src/main/java/ch/heigvd/pro/a03/Server.java
@@ -4,15 +4,17 @@ import ch.heigvd.pro.a03.httpServer.HttpServer;
 import ch.heigvd.pro.a03.socketServer.SocketServer;
 import ch.heigvd.pro.a03.utils.Protocole;
 
+import static spark.Spark.secure;
+
 public class Server{
 
 
 
     public static void main(String[] args) {
 
-        //String keyStoreLocation = "deploy/keystore.jks";
-        //String keyStorePassword = "pro2019heig";
-        //secure(keyStoreLocation, keyStorePassword, null, null);
+        String keyStoreLocation = "towerdefense-server/deploy/keystore.jks";
+        String keyStorePassword = "pro2019heig";
+        secure(keyStoreLocation, keyStorePassword, null, null);
         // Run HTTP on other thread;
         new Thread(new HttpServer(Protocole.HTTPSERVERPORT)).start();
         new Thread(new SocketServer(Protocole.SOCKETSERVERPORT)).start();
