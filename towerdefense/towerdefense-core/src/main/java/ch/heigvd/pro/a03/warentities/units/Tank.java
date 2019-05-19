1bfc3ce1464d430cfae8905fe96c023dfe95cae9
andy <andres.moreno@heig-vd.ch>
2019-05-19 21:26:29 +0200

    javadoc units

--- towerdefense/towerdefense-core/src/main/java/ch/heigvd/pro/a03/warentities/units/Tank.java
+++ towerdefense/towerdefense-core/src/main/java/ch/heigvd/pro/a03/warentities/units/Tank.java
@@ -4,8 +4,23 @@ import ch.heigvd.pro.a03.warentities.WarEntityType;
 
 import java.awt.*;
 
+/***
+ * Class representing a Tank
+ * @Author Andres Moreno
+ */
 public class Tank extends Unit {
 
+    /***
+     * Constructor
+     * @param name the unit name
+     * @param position the position at the grid
+     * @param totalHealth total health points
+     * @param defPoint defense points
+     * @param attackCoolDown attack cool down
+     * @param attackPoints attack points
+     * @param range
+     * @param price the cost in order to buy it
+     */
     public Tank(String name, Point position, int totalHealth,
                 int defPoint, int attackCoolDown, int speed,
                 int attackPoints, int range, int price) {
@@ -16,8 +31,13 @@ public class Tank extends Unit {
 
     }
 
+    /***
+     * Constructor
+     * @param position the position at the grid
+     */
     public Tank(Point position){
-        this("Tank", position,500,500,10,15,500,4,500);
+        this("Tank", position,500,500,10,15,
+                500,4,500);
     }
 
     @Override
