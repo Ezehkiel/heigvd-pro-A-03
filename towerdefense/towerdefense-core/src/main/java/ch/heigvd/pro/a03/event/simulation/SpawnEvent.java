d4f9b2d1798bfbde5b2c4764f71c32e4a01422c7
Didier Page <page.didier@gmail.com>
2019-05-23 20:58:49 +0200

    <refactor><comment> forgot a class

--- towerdefense/towerdefense-core/src/main/java/ch/heigvd/pro/a03/event/simulation/SpawnEvent.java
+++ towerdefense/towerdefense-core/src/main/java/ch/heigvd/pro/a03/event/simulation/SpawnEvent.java
@@ -4,12 +4,23 @@ import ch.heigvd.pro.a03.warentities.WarEntityType;
 
 import java.awt.*;
 
-
+/**
+ * Spawn event is when a unit is create on the map
+ * @Author Didier Page
+ */
 public class SpawnEvent extends SimEvent {
 
     public final WarEntityType.UnitType UNIT_TYPE;
     public final Point SPAWN_POINT;
 
+    /**
+     * Constructor
+     * @param ticId The id of the simulation tic
+     * @param entityId the entity id (unit)
+     * @param unitType the type of unit
+     * @param spawnPoint the position where the unit spawn
+     * @param map_id the id of the map where the event appends
+     */
     public SpawnEvent(int ticId, int entityId, WarEntityType.UnitType unitType, Point spawnPoint,int map_id) {
         super(ticId, SimEventType.SPAWN, entityId, map_id);
         this.UNIT_TYPE = unitType;
